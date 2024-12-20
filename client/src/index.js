89897cd517625f7e50e1c0c4019d3d1c73103037
Ryan Marando <marandoryan@gmail.com>
2024-12-20 14:44:03 -0500

    added analytics

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,12 @@ import "./index.css";
 import WeatherInputForm from "./pages/PostWeatherInput";
 import Admin from "./pages/Admin";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <Analytics />
         <Router>
             <Routes>
                 <Route path="/" element={<WeatherInputForm />} />
