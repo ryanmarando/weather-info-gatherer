df61164d837a0e3538733a27eb112956d381c294
Ryan Marando <marandoryan@gmail.com>
2024-12-29 15:34:14 -0500

    first deployment

--- api-backend/src/app.ts
+++ api-backend/src/app.ts
@@ -9,7 +9,7 @@ import errors from "./middleware/errors.js";
 import authenticated from "./middleware/auth.js";
 
 const app = express();
-const port = 3000;
+const port = 8080; // 3000
 
 app.use(express.json());
 app.use(cors());
@@ -17,16 +17,16 @@ app.use(cors());
 app.use(xss);
 app.use(logging.logRequest);
 
+app.get("/", (req, res) => {
+    res.send("Welcome To The Weather Data Input Machine!");
+});
+
 app.use("/auth", authRouter);
 app.use(authenticated);
 
 app.use("/inputs", inputsRouter);
 app.use("/admins", adminsRouter);
 app.use(errors.errorHandler);
-
-app.get("/", (req, res) => {
-    res.send("Welcome To The Weather Data Input Machine!");
-});
 
 app.listen(port, "0.0.0.0", () => {
     console.log(`API listening on http://localhost:${port}`);
